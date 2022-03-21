c50bcdbbecb13145fcd08d1dc7e633f5597bee24
htak <hitoshi.takemori.pinokonoko@gmail.com>
2022-03-22 00:12:13 +0900

    first commit and  started SwiftUI Essentials
    finished Section3 in Using Stacks to Arrange Views

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Hito4TK on 2022/03/21.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 0.25, total: 1.0)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.filled")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.tophalf.filled")
+                }
+            }
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action:{}) {
+                    Image(systemName: "forward.fill")
+                }
+            }
+        }
+        .padding()
+//        ProgressView(
+//        Text("Hello, world!")
+//            .padding()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Hito4TK on 2022/03/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
