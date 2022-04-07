6bd8630e1fcde7b894a55424144caaea0548f1c1
htak <hitoshi.takemori.pinokonoko@gmail.com>
2022-04-07 22:41:10 +0900

    finished Persisting Data in Chapter 6

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -20,19 +20,20 @@ struct ScrumsView: View {
             ForEach($scrums) { $scrum in
                 NavigationLink(destination: DetailView(scrum: $scrum)) {
                     CardView(scrum: scrum)
-                        
+                    
                 }
                 .listRowBackground(scrum.theme.mainColor)
             }
-            .navigationTitle("Daily Scrums")
-            .toolbar {
-                Button(action: {
-                    isPresentingNewScrumView = true
-                }) {
-                    Image(systemName: "plus")
-                }
-                .accessibilityLabel("New Scrum")
+
+        }
+        .navigationTitle("Daily Scrums")
+        .toolbar {
+            Button(action: {
+                isPresentingNewScrumView = true
+            }) {
+                Image(systemName: "plus")
             }
+            .accessibilityLabel("New Scrum")
         }
         .sheet(isPresented: $isPresentingNewScrumView) {
             NavigationView {
